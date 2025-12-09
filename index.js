2e492e2a6142f5b3a3f48ea9b3a531df068f1db1
Mohammed- Hamim <mohammedhamim0158@gmail.com>
2025-12-10 01:17:36 +0600

    a get api is made for getting all issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
 
@@ -13,6 +13,45 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('CitySheba server is  running ')
 })
+
+
+const uri = process.env.URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db('city-sheba-db')
+        const issuesCollection = db.collection("all-issue")
+
+
+
+        // get all issue apis
+        app.get('/all-issue', async (req, res) => {
+            const cursor = issuesCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log(`CitySheba listening on port ${port}`)
